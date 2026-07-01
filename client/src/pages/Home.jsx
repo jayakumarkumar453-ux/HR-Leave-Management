3a363a61c5c58d59161dfd0b1a7cec7b4101759c
jayakumarkumar453-ux <jayakumarkumar453@gmail.com>
2026-07-01 20:21:42 +0530

    Create Home.jsx

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,23 @@
+function Home() {
+
+    return (
+
+        <div style={{ padding: "30px" }}>
+
+            <h1>HR Employee Leave Management System</h1>
+
+            <h3>Welcome Employee</h3>
+
+            <p>
+
+                Apply leave easily using React and Node.js.
+
+            </p>
+
+        </div>
+
+    );
+
+}
+
+export default Home;
